f1d6d1403cb443043c41932097f4384d8dbcfa84
dayounglee <verdantjuly@kakao.com>
2024-10-25 12:35:18 +0900

    [Feat] ejs

--- app.js
+++ app.js
@@ -11,4 +11,14 @@ app.get("/", (req, res) => {
   res.render("index", { title: "안녕하세요", message: "반갑습니다." });
 });
 
+const posts = [
+  { title: "테스트 타이틀 1", content: "테스트 컨텐트 1" },
+  { title: "테스트 타이틀 1", content: "테스트 컨텐트 1" },
+  { title: "테스트 타이틀 1", content: "테스트 컨텐트 1" },
+];
+
+app.get("/sample", (req, res) => {
+  res.render("sample", { data: posts });
+});
+
 app.listen(PORT);
